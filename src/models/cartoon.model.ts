d20db5e154155d328b7f9a2487b82dba57970f91
dsunegin <dsunegin@gmail.com>
2021-07-11 11:05:42 +0300

    Initial commit

---
+++ src/models/cartoon.model.ts
@@ -0,0 +1,173 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {idInjection: false, mysql: {schema: 'kid', table: 'cartoon'}}})
+export class Cartoon extends Entity {
+  @property({
+    type: 'number',
+    required: true,
+    precision: 10,
+    scale: 0,
+    id: 1,
+    mysql: {columnName: 'id', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'N'},
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    length: 255,
+    mysql: {columnName: 'title', dataType: 'varchar', dataLength: 255, dataPrecision: null, dataScale: null, nullable: 'N'},
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    length: 255,
+    mysql: {columnName: 'alias', dataType: 'varchar', dataLength: 255, dataPrecision: null, dataScale: null, nullable: 'N'},
+  })
+  alias: string;
+
+  @property({
+    type: 'string',
+    length: 65535,
+    mysql: {columnName: 'video', dataType: 'text', dataLength: 65535, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  video?: string;
+
+  @property({
+    type: 'string',
+    length: 65535,
+    mysql: {columnName: 'image', dataType: 'text', dataLength: 65535, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  image?: string;
+
+  @property({
+    type: 'string',
+    length: 65535,
+    mysql: {columnName: 'description', dataType: 'text', dataLength: 65535, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  description?: string;
+
+  @property({
+    type: 'date',
+    mysql: {columnName: 'created', dataType: 'datetime', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  created?: string;
+
+  @property({
+    type: 'date',
+    mysql: {columnName: 'publish_up', dataType: 'datetime', dataLength: null, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  publishUp?: string;
+
+  @property({
+    type: 'string',
+    length: 65535,
+    mysql: {columnName: 'url', dataType: 'text', dataLength: 65535, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  url?: string;
+
+  @property({
+    type: 'string',
+    length: 65535,
+    mysql: {columnName: 'youtube_channel', dataType: 'text', dataLength: 65535, dataPrecision: null, dataScale: null, nullable: 'Y'},
+  })
+  youtubeChannel?: string;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'ru', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  ru?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'uk', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  uk?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'be', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  be?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'pl', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  pl?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'en', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  en?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'fr', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  fr?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'de', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  de?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'es', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  es?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'it', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  it?: number;
+
+  @property({
+    type: 'number',
+    precision: 10,
+    scale: 0,
+    mysql: {columnName: 'bg', dataType: 'int', dataLength: null, dataPrecision: 10, dataScale: 0, nullable: 'Y'},
+  })
+  bg?: number;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Cartoon>) {
+    super(data);
+  }
+}
+
+export interface CartoonRelations {
+  // describe navigational properties here
+}
+
+export type CartoonWithRelations = Cartoon & CartoonRelations;
